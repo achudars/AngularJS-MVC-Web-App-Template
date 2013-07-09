f3c1ce6c58c34fee6eb610331d284c52db41c6c7
Aleks <achudars@gmail.com>
2013-07-09 13:47:19 +0100

    Update routes.js

--- controller/routes.js
+++ controller/routes.js
@@ -1,15 +1,1 @@
-var newConfig = function($routeProvider) { 
-    $routeProvider
-        .when('/view1', {
-            controller: 'controllers',
-            templateUrl: 'view/view1.html'
-        })
-        .otherwise({
-            controller: 'controllers',
-            templateUrl: 'view/view2.html'
-        })
-    ;
-};
-
-var SeedApp = angular.module('SeedApp',[]).config(newConfig);
-
+"use strict";
