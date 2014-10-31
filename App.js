6b5fc3312d9fbe7ca35c8c1db5a80aeb963a5521
Aleks <achudars@gmail.com>
2014-10-31 09:39:54 +0000

    Encapsulated controller and app into an anonymous function

--- App.js
+++ App.js
@@ -1,23 +1,25 @@
-"use strict";
+(function() {
 
-var App = angular.module("App", [
-  "App.controllers",
-  "App.services",
-  "App.directives",
-  "App.filters",
-  "ngRoute",
-  "ngResource"
-]);
+  "use strict";
 
-App.config(function ($routeProvider) {
+  var App = angular.module("App", [
+    "App.controllers",
+    "App.services",
+    "App.directives",
+    "App.filters",
+    "ngRoute",
+    "ngResource"
+  ]);
+
+  App.config(function ($routeProvider) {
     $routeProvider
-        .when('/view1', {
-             templateUrl: 'view/view1.html'
-        })
-        .when('/view2', {
-             templateUrl: 'view/view2.html'
-        })
-        .otherwise({redirectTo : 'view1'})
-    ;
-});
+      .when('/view1', {
+           templateUrl: 'view/view1.html'
+      })
+      .when('/view2', {
+           templateUrl: 'view/view2.html'
+      })
+      .otherwise({redirectTo : 'view1'});
+  });
 
+}());
--- controller/controllers.js
+++ controller/controllers.js
@@ -1,25 +1,16 @@
-"use strict";
+(function() {
 
-var App = angular.module("App.controllers", []);
+  "use strict";
 
-App.controller("MyCtrl1", ["$scope", function ($scope, UtilSrvc){
+  var App = angular.module("App.controllers", []);
+
+  App.controller("MyCtrl1", ["$scope", function ($scope, UtilSrvc){
     $scope.aVariable = 'anExampleValueWithinScope';
     $scope.valueFromService = UtilSrvc.helloWorld("User");
-}]);
+  }]);
 
-App.controller("MyCtrl2", ["$scope", function($scope){
+  App.controller("MyCtrl2", ["$scope", function($scope){
 
-}]);
+  }]);
 
-// you may add more controllers below
-
-/*'use strict';
-
-App.controller("MyCtrl1" ,function ($scope, UtilSrvc) {
-    $scope.aVariable = 'anExampleValueWithinScope';
-    $scope.valueFromService = UtilSrvc.helloWorld("User");
-});
-
-App.controller("MyCtrl2" ,function ($scope) {
-
-});*/
+}());
